287dfb96efd48233de351475776f6cb2b33a52e2
AmZaDin14 <amrireza97@gmail.com>
2020-12-02 17:02:38 +0700

    revert to sqlite3

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -81,16 +81,18 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'starlights-database',
-        'USER': 'amri@starlights',
-        'PASSWORD': '@mrireza2003',
-        'HOST': 'starlights.database.windows.net',
-        'PORT': '1433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'sql_server.pyodbc',
+        # 'NAME': 'starlights-database',
+        # 'USER': 'amri@starlights',
+        # 'PASSWORD': '@mrireza2003',
+        # 'HOST': 'starlights.database.windows.net',
+        # 'PORT': '1433',
         
-        'OPTIONS': {
-            'driver': '{ODBC Driver 13 for SQL Server}',
-        }
+        # 'OPTIONS': {
+        #     'driver': '{ODBC Driver 13 for SQL Server}',
+        # }
     }
 }
 
